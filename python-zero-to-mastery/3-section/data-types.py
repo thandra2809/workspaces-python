e8961ed5d5e415c2e705c65c293bfea7cd50ffe2
Srinivasa Rao THANDRA <thandra82@gmail.com>
2020-07-21 00:49:41 -0700

    Started on new python course

---
+++ python-zero-to-mastery/3-section/data-types.py
@@ -0,0 +1,16 @@
+# Fundamental Data Types
+# int
+# float
+# bool
+# str
+# list
+# tuple
+# set
+# dict
+
+# Classes -> Custom Data Types
+
+# Specialized Data Types  -> Example Modules
+
+None
+
---
+++ python-zero-to-mastery/3-section/int-float-datatype.py
@@ -0,0 +1,11 @@
+i = 3
+print(i)
+print(type(i))
+
+print(2+4)
+print(2-4)
+print(2*4)
+print(2/4)
+
+print(type(2/4))
+
