178fde0319921deeb82ffe250f1c7b3cdc055313
Srinivasa Rao THANDRA <thandra82@gmail.com>
2020-11-23 21:43:06 -0800

    Added new practice files from linked-in

--- python-essentials/Chap14/modules.py
+++ python-essentials/Chap14/modules.py
@@ -1,10 +1,13 @@
 #!/usr/bin/env python3
 # Copyright 2009-2017 BHG http://bw.org/
 
-import sys
+import os
+
 
 def main():
-    v = sys.version_info
-    print('Python version {}.{}.{}'.format(*v))
+    v = os.getenv('PATH').split(':')
+    print(v)
 
-if __name__ == '__main__': main()
+
+if __name__ == '__main__':
+    main()
