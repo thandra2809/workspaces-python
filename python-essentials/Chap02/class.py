916ca45e0459a49b8bb059564f84503455a8d0c5
Srinivasa Rao THANDRA <thandra82@gmail.com>
2020-11-28 18:58:00 -0800

    Added new practice files from linked-in

--- python-essentials/Chap02/class.py
+++ python-essentials/Chap02/class.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 # Copyright 2009-2017 BHG http://bw.org/
 
+
 class Duck:
     def quack(self):
         print('Quaaack!')
@@ -8,9 +9,11 @@ class Duck:
     def walk(self):
         print('Walks like a duck.')
 
+
 def main():
     donald = Duck()
     donald.quack()
     donald.walk()
+
 
 if __name__ == '__main__': main()
